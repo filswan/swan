9281d7f5fb9438880ad7bb1f66187c30fdf015ba
chi <czhang@nbai.io>
2021-02-18 11:58:45 -0500

    1. read config from DealConf 2. user simple logging 3. acquire reals from deal_list and metadata csv

--- client/task_sender/service/deal.py
+++ client/task_sender/service/deal.py
@@ -4,67 +4,26 @@ import math
 import os
 import re
 import subprocess
-#   365 days
 import time
 from decimal import Decimal
 
 from common.OfflineDeal import OfflineDeal
 
-logging.config.fileConfig('logging.conf')
-
-# create logger
-logger = logging.getLogger(__name__)
-
-MINERS = ['']
-SENDER_WALLET = ""
-MAX_PRICE = '0.000000018'
-IS_VERIFIED_DEAL = True
+logging.basicConfig(level=logging.DEBUG)
 
 DURATION = '1051200'
-EPOCH_INTERVAL_HOURS = 96
 EPOCH_PER_HOUR = 120
 
 
 # 32GB
 # PIECE_SIZE = '34091302912'
 
-
-def get_current_epoch() -> int:
-    current_epoch = None
-    todo_epoch = None
-
-    # 'lotus sycn wait'
-    #   return immediately if node is synced
-    #   wait for synchronization if node is not synced
-    try:
-        proc = subprocess.check_output(['lotus', 'sync', 'wait'], timeout=3)
-        lines = proc.rstrip().decode('utf-8').split('\n')
-    except subprocess.TimeoutExpired as e:
-        print(e)
-        lines = e.output.rstrip().decode('utf-8').split('\n')
-    for line in lines:
-        current_epoch_match = re.findall(r'''Target: (\d+)''', line)
-        todo_epoch_match = re.findall(r'''Todo: (\d+)''', line)
-        if len(current_epoch_match) > 0:
-            current_epoch = int(current_epoch_match[0])
-        if len(todo_epoch_match) > 0:
-            todo_epoch = int(todo_epoch_match[0])
-
-        if current_epoch and todo_epoch is not None:
-            if todo_epoch < 5:
-                break
-            else:
-                logger.error("node not synced")
-                exit(1)
-        else:
-            continue
-
-    # current_epoch might be 0 occasional
-    if current_epoch == 0:
-        logger.error("failed to get current epoch, value 0")
-        exit(1)
-
-    return current_epoch
+class DealConf:
+    miner_id = None
+    sender_wallet = None
+    max_price = None
+    is_verified_deal = None
+    epoch_interval_hours = None
 
 
 def get_current_epoch_by_current_time():
@@ -82,8 +41,8 @@ def get_miner_price(miner_fid: str):
                                        stderr=subprocess.PIPE)
 
     except Exception as e:
-        logger.error(e)
-        logger.warning('lotus query-ask process not success.')
+        logging.error(e)
+        logging.warning('lotus query-ask process not success.')
         return
 
     if proc is None:
@@ -92,7 +51,7 @@ def get_miner_price(miner_fid: str):
         lines = proc.rstrip().decode('utf-8').split('\n')
 
         for line in lines:
-            logger.info('----- info: %s' % line)
+            logging.info('----- info: %s' % line)
             price_match = re.findall(r'''^Price per GiB: ([0-9]*\.?[0-9]+) FIL''', line)
             if len(price_match) != 0:
                 price = price_match[0]
@@ -104,17 +63,17 @@ def get_miner_price(miner_fid: str):
                 'verified_price': verified_price}
 
 
-def propose_offline_deal(_miner, _price, piece_size, data_cid, piece_cid):
-    start_epoch = get_current_epoch_by_current_time() + (EPOCH_INTERVAL_HOURS + 1) * EPOCH_PER_HOUR
-    command = ['lotus', 'client', 'deal', '--from', SENDER_WALLET, '--start-epoch', str(start_epoch),
-               '--manual-piece-cid', piece_cid, '--manual-piece-size', piece_size, data_cid, _miner, _price, DURATION]
-    logger.info(command)
+def propose_offline_deal(_price, piece_size, data_cid, piece_cid, deal_conf: DealConf):
+    start_epoch = get_current_epoch_by_current_time() + (deal_conf.epoch_interval_hours + 1) * EPOCH_PER_HOUR
+    command = ['lotus', 'client', 'deal', '--from', deal_conf.sender_wallet, '--start-epoch', str(start_epoch),
+               '--manual-piece-cid', piece_cid, '--manual-piece-size', piece_size, data_cid, deal_conf.miner_id, _price,
+               DURATION]
+    logging.info(command)
     input("Press Enter to continue...")
-    # proc = subprocess.Popen(command, stdout=subprocess.PIPE)
-    # resp = proc.stdout.readline().rstrip().decode('utf-8')
-    # deal_cid = resp
-    deal_cid = 'deal_cid'
-    logger.info('deal cid: %s, start epoch: %s' % (deal_cid, start_epoch))
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+    resp = proc.stdout.readline().rstrip().decode('utf-8')
+    deal_cid = resp
+    logging.info('deal cid: %s, start epoch: %s' % (deal_cid, start_epoch))
     return [deal_cid, start_epoch]
 
 
@@ -131,57 +90,67 @@ def calculate_piece_size_from_file_size(_file_size):
     return real_piece_size
 
 
-def new(csv_file_path=None, deal_list=None):
+def send_deals_to_miner(deal_conf: DealConf, csv_file_path=None, deal_list=None):
     attributes = [i for i in OfflineDeal.__dict__.keys() if not i.startswith("__")]
 
+    # todo init csv_file_path when deals are from deal_list
     output_csv_path = csv_file_path + ".output"
     if deal_list:
         pass
     else:
+        deal_list = []
         with open(csv_file_path, "r") as csv_file:
             fieldnames = attributes
 
-        reader = csv.DictReader(csv_file, delimiter=',', fieldnames=fieldnames)
-        next(reader, None)
+            reader = csv.DictReader(csv_file, delimiter=',', fieldnames=fieldnames)
+            next(reader, None)
+            for row in reader:
+                deal = OfflineDeal()
+                for attr in row.keys():
+                    deal.__setattr__(attr, row.get(attr))
+                deal_list.append(deal)
 
     for _deal in deal_list:
-        for miner in MINERS:
-            data_cid = _deal.data_cid
-            piece_cid = _deal.piece_cid
-            source_file_url = _deal.source_file_url
-            md5 = _deal.car_file_md5
-            file_size = _deal.source_file_size
-            prices = get_miner_price(miner)
 
-            if prices:
-                if IS_VERIFIED_DEAL:
-                    price = prices['verified_price']
-                else:
-                    price = prices['price']
+        data_cid = _deal.data_cid
+        piece_cid = _deal.piece_cid
+        source_file_url = _deal.source_file_url
+        md5 = _deal.car_file_md5
+        file_size = _deal.source_file_size
+        prices = get_miner_price(deal_conf.miner_id)
+
+        if prices:
+            if deal_conf.is_verified_deal:
+                price = prices['verified_price']
             else:
-                continue
+                price = prices['price']
+        else:
+            continue
 
-            if Decimal(price).compare(Decimal(MAX_PRICE)) > 0:
-                logger.warning("miner %s price %s higher than max price %s" % (miner, price, MAX_PRICE))
-                continue
+        if Decimal(price).compare(Decimal(deal_conf.max_price)) > 0:
+            logging.warning(
+                "miner %s price %s higher than max price %s" % (deal_conf.miner_id, price, deal_conf.max_price))
+            continue
 
-            piece_size = str(calculate_piece_size_from_file_size(file_size))
-            result = propose_offline_deal(miner, price, piece_size, data_cid, piece_cid)
-            _deal_cid = result[0]
-            _start_epoch = result[1]
+        piece_size = str(calculate_piece_size_from_file_size(file_size))
 
-            file_exists = os.path.isfile(output_csv_path)
-            with open(output_csv_path, "a") as output_csv_file:
-                output_fieldnames = ['miner_id', 'file_source_url', 'md5', 'start_epoch', 'deal_cid', 'timestamp']
-                csv_writer = csv.DictWriter(output_csv_file, delimiter=',', fieldnames=output_fieldnames)
-                if not file_exists:
-                    csv_writer.writeheader()
-                csv_data = {
-                    'miner_id': miner,
-                    'file_source_url': source_file_url,
-                    'md5': md5,
-                    'start_epoch': _start_epoch,
-                    'deal_cid': _deal_cid,
-                    'timestamp': int(time.time() * 1000)
-                }
-                csv_writer.writerow(csv_data)
+        # todo price here is price/Gb, need to calculate according to sector size
+        result = propose_offline_deal(price, piece_size, data_cid, piece_cid, deal_conf)
+        _deal_cid = result[0]
+        _start_epoch = result[1]
+
+        file_exists = os.path.isfile(output_csv_path)
+        with open(output_csv_path, "a") as output_csv_file:
+            output_fieldnames = ['miner_id', 'file_source_url', 'md5', 'start_epoch', 'deal_cid', 'timestamp']
+            csv_writer = csv.DictWriter(output_csv_file, delimiter=',', fieldnames=output_fieldnames)
+            if not file_exists:
+                csv_writer.writeheader()
+            csv_data = {
+                'miner_id': deal_conf.miner_id,
+                'file_source_url': source_file_url,
+                'md5': md5,
+                'start_epoch': _start_epoch,
+                'deal_cid': _deal_cid,
+                'timestamp': int(time.time() * 1000)
+            }
+            csv_writer.writerow(csv_data)
